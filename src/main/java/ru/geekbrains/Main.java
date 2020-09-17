211ebd6216515e01fa99da72c5d6e29e2e139c11
kornev <aakor23@gmail.com>
2020-09-17 12:09:18 +0300

    HW3

--- src/main/java/ru/geekbrains/Main.java
+++ src/main/java/ru/geekbrains/Main.java
@@ -1,100 +1,100 @@
 package ru.geekbrains;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        /* Задание 2.1
-        На основе программного кода из домашнего задания №1 реализуйте массив на основе существующих примитивных
-        или ссылочных типов данных.
-        Выполните обращение к массиву и базовые операции класса Arrays.
-        Оценить выполненные методы с помощью базового класса System.nanoTime().
+        /* Задание 3.1
+        На основе массива из домашнего задания 2.1 реализуйте простой список и коллекцию.
+        Оцените время выполнения преобразования.
         */
-        int[] simpleTypeArray = new int[5000];
+        final int ARR_LENGTH = 500;
+        int[] simpleTypeArray = new int[ARR_LENGTH];
         int[] copyOfSimpleTypeArray = null;
         fillArray(simpleTypeArray);
 
         long timeStart = System.nanoTime();
-        copyOfSimpleTypeArray = Arrays.copyOf(simpleTypeArray, simpleTypeArray.length);
-        System.out.println("Затраты времени на копирование массива: " + (System.nanoTime() - timeStart) + " наносекунд.");
-        System.out.println("Массив 1: " + Arrays.toString(simpleTypeArray));
-        System.out.println("Массив 2: " + Arrays.toString(copyOfSimpleTypeArray));
-
-        timeStart = System.nanoTime();
-        Arrays.sort(simpleTypeArray);
-        System.out.println("Затраты времени на сортировку: " + (System.nanoTime() - timeStart) + " наносекунд.");
-        System.out.println("Отсортированный массив: " + Arrays.toString(simpleTypeArray));
-
-        /*
-        Задание 2.2
-        На основе написанного кода в задании 2.1 реализуйте линейный и двоичный поиск.
-        Оценить алгоритмы линейного и двоичного поиска с помощью базового класса System.nanoTime(),
-        при необходимости расширьте уже существующий массив данных.
-         */
-
-        //Линейный поиск перебором
-        timeStart = System.nanoTime();
-        System.out.println("Элемент " + copyOfSimpleTypeArray[50] + " найден как элемент с индексом " + linearSearch(copyOfSimpleTypeArray[50], simpleTypeArray));
-        System.out.println("Затраты времени на линейный поиск: " + (System.nanoTime() - timeStart) + " наносекунд.");
-
-        //Двоичный поиск
-        timeStart = System.nanoTime();
-        System.out.println("Элемент " + copyOfSimpleTypeArray[50] + " найден как элемент с индексом " + Arrays.binarySearch(simpleTypeArray, copyOfSimpleTypeArray[50]));
-        System.out.println("Затраты времени на двоичный поиск: " + (System.nanoTime() - timeStart) + " наносекунд.");
+        ArrayList<Integer> arrList = new ArrayList<>(500);
+        for (int i = 0; i < simpleTypeArray.length; i++) {
+            arrList.add(simpleTypeArray[i]);
+        }
+        long timeStop = System.nanoTime();
+        System.out.println("Время заполнения ArrayList'a " + (timeStop - timeStart) + " наносекунд.");
+        Collections.sort(arrList);
+        System.out.println(arrList);
 
         /*
-        Задание 2.3
-        Создайте массив размером 400 элементов.
-        Выполните сортировку с помощью метода sort().
-        Оцените сортировку с помощью базового класса System.nanoTime().
+        Задание 3.2
+        На основе списка из задания 3.1 реализуйте основные методы добавления, удаления и получения объекта
+        или элемента из списка.
+        Оценить выполненные методы с помощью базового класса System.nanoTime().
          */
-        int[] arr = new int[400];
-        fillArray(arr);
-
-        int[] arr2, arr3, arr4;
-        arr2 = Arrays.copyOf(arr, arr.length);
-        arr3 = Arrays.copyOf(arr, arr.length);
-        arr4 = Arrays.copyOf(arr, arr.length);
+        timeStart = System.nanoTime();
+        arrList.add(999);
+        timeStop = System.nanoTime();
+        System.out.println("Время добавления элемента в ArrayList " + (timeStop - timeStart) + " наносекунд.");
 
         timeStart = System.nanoTime();
-        Arrays.sort(arr);
-        System.out.println("Затраты времени на стандартную сортировку: " + (System.nanoTime() - timeStart) + " наносекунд.");
-        System.out.println("Отсортированный массив: " + Arrays.toString(arr));
+        System.out.println(arrList.get(500));
+        timeStop = System.nanoTime();
+        System.out.println("Время получения элемента из ArrayList " + (timeStop - timeStart) + " наносекунд.");
+
+        timeStart = System.nanoTime();
+        arrList.remove(500);
+        timeStop = System.nanoTime();
+        System.out.println("Время удаления элемента из ArrayList " + (timeStop - timeStart) + " наносекунд.");
 
         /*
-        Задание 2.4
-        На основе существующего массива данных из задания 2.3 реализуйте алгоритм сортировки пузырьком.
-        Оцените сортировку с помощью базового класса System.nanoTime().
-        Сравните время выполнения алгоритмы сортировки методом sort() из задания 2.1 и сортировку пузырьком.
+        Задание 3.3
+        Реализуйте простой односвязный список и его базовые методы.
          */
-        timeStart = System.nanoTime();
-        bubbleSort(arr2);
-        System.out.println("Затраты времени на сортировку пузырьком: " + (System.nanoTime() - timeStart) + " наносекунд.");
-        System.out.println("Отсортированный массив: " + Arrays.toString(arr2));
+        ListSimpleLinked listSimpleLinked = new ListSimpleLinked();
+        listSimpleLinked.addFront("A");
+        listSimpleLinked.addBack(2);
+        listSimpleLinked.printList();
+        listSimpleLinked.removeElemenet(2);
+        listSimpleLinked.removeElemenet("A");
+        listSimpleLinked.printList();
 
         /*
-        Задание 2.5
-        На основе массива данных из задания 2.3 реализуйте алгоритм сортировки методом выбора.
-        Оцените сортировку с помощью базового класса System.nanoTime().
-        Сравните с временем выполнения алгоритмов сортировки из прошлых заданий 2.3 и 2.4.
-         */
-        timeStart = System.nanoTime();
-        choiceSort(arr3);
-        System.out.println("Затраты времени на сортировку методом выбора: " + (System.nanoTime() - timeStart) + " наносекунд.");
-        System.out.println("Отсортированный массив: " + Arrays.toString(arr3));
+        Задание 3.4
+        На основе списка из задания 3.1 (наверное, все же 3.3?) реализуйте простой двусторонний список и
+        его базовые методы.
+        Реализуйте список заполненный объектами из вашего класса из задания 1.3
+        */
+        System.out.println("\nДвусторонний список:");
+        ListLinked listLinked = new ListLinked();
+        listLinked.addFront(new MyClass("Alex"));
+        listLinked.addFront(new MyClass("Bob"));
+
+        listLinked.printList();
+        System.out.println();
+        listLinked.printReversedList();
 
         /*
-        Задание 2.6
-        На основе массива данных из задания 2.3 реализуйте алгоритм сортировки методом вставки.
-        Оцените сортировку с помощью базового класса System.nanoTime().
-        Сравните с временем выполнения алгоритмов сортировки из прошлых заданий 2.3, 2.4 и 2.5.
+        Задание 3.5
+        Реализуйте итератор на основе связанных списков из задания 3.4 и выполните базовые операции итератора.
+        Оцените время выполнения операций с помощью базового метода System.nanoTime()
          */
+        LinkedList<MyClass> linkedList = new LinkedList<>();
+        linkedList.add(new MyClass("Alex"));
+        linkedList.add(new MyClass("Bob"));
+        linkedList.add(new MyClass("Alice"));
+        Iterator iterator = linkedList.iterator();
+
+        System.out.println();
+
         timeStart = System.nanoTime();
-        insertSort(arr4);
-        System.out.println("Затраты времени на сортировку методом вставки: " + (System.nanoTime() - timeStart) + " наносекунд.");
-        System.out.println("Отсортированный массив: " + Arrays.toString(arr4));
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().toString());
+            iterator.remove();
+        }
+        timeStop = System.nanoTime();
+        System.out.println("Время на перебор и удаление элементов итератором " + (timeStop - timeStart) + " наносекунд.");
+
+        System.out.println("Теперь список такой: " + linkedList.toString());
+
     }
 
     private static void fillArray(int[] arr) {
@@ -102,63 +102,6 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             arr[i] = r.nextInt(arr.length * 2);
         }
-    }
-
-    private static void insertSort(int[] arr) {
-        int buff;
-        int lastJ;
-        for (int i = 1; i < arr.length; i++) {
-            buff = arr[i];
-            lastJ = i;
-            for (int j = i; j > 0; j--) {
-                if (buff < arr[j - 1]) {
-                    arr[j] = arr[j - 1];
-                    lastJ = j;
-                } else {
-                    break;
-                }
-            }
-            arr[lastJ - 1] = buff;
-        }
-    }
-
-    private static void choiceSort(int[] arr) {
-        int minIndex = 0;
-        int buff;
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = i; j < arr.length; j++) {
-                if (arr[minIndex] > arr[j]) {
-                    minIndex = j;
-                }
-            }
-            buff = arr[i - 1];
-            arr[i - 1] = arr[minIndex];
-            arr[minIndex] = buff;
-            minIndex = i;
-        }
-    }
-
-    private static void bubbleSort(int[] arr) {
-        int buff;
-        for (int j = 1; j < arr.length; j++) {
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] > arr[i + 1]) {
-                    buff = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = buff;
-                }
-            }
-
-        }
-    }
-
-    private static int linearSearch(int intToSearch, int[] Array) {
-        for (int i = 0; i < Array.length; i++) {
-            if (intToSearch == Array[i]) {
-                return i;
-            }
-        }
-        return -1;
     }
 
     static class MyClass {
